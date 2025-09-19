ec124347a0b60d16d5cf1403337d2a79f57cd01d
Makgomo-77 <makgomoshayi@gmail.com>
2025-09-19 13:31:40 +0200

    initial commit

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+import search from "./components/search";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Search">
+      <h1>Github User Search</h1>
+      <search />
+    </div>
+  );
+}
+
+export default App;
+  
