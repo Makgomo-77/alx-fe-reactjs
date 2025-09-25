d8e6174353e05c89f58561edf28d6e9cd1bcd508
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-09-25 13:32:21 +0200

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,7 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
-
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <UserProfile />
+    </div>
+  )
+}
 
 export default App
+
