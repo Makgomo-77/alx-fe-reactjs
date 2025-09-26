0c436e724179aad3993276ebdc959f2470cf25ca
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-09-26 13:04:12 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -19,7 +19,7 @@ function UserProfile() {
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:max-w-sm"
       />
       <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">
         John Doe
