7f6d797550e32d91111131e3a33f404be04d317d
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-09-26 13:12:16 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -15,7 +15,7 @@ function UserProfile() {
 }
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-16 md:max-w-sm">
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
