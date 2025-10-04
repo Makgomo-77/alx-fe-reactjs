0cc2a2c17081fc5014b4d8ee72d30db0d4266db0
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-10-04 08:20:07 +0200

    Update vite.config.js

--- recipe-sharing-platform/vite.config.js
+++ recipe-sharing-platform/vite.config.js
@@ -5,3 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
